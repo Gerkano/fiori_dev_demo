a990d0ae13068cec506028536d946b4ae36e5ba3
Gerald Chitrov <gerald_chitrov@atlantconsult.com>
2023-12-07 09:56:13 +0000

    Added pdf form to dump app

--- dumps/webapp/controller/View1.controller.js
+++ dumps/webapp/controller/View1.controller.js
@@ -9,19 +9,25 @@ sap.ui.define([
     'sap/m/Token',
 	'sap/m/MessageToast',
     'sap/ui/core/Fragment',
+	'sap/m/PDFViewer',
     '../model/formatter'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
 
-    function (Controller, Device, Filter, FilterOperator, Sorter, Menu, MenuItem, Token, MessageToast, Fragment, formatter) {
+    function (Controller, Device, Filter, FilterOperator, Sorter, Menu, MenuItem, Token, MessageToast, Fragment, PDFViewer, formatter) {
         "use strict";
         
         return Controller.extend("dumpsShow.dumps.controller.View1", {
 
             formatter: formatter,
             onInit: function () { 
+                // var oModel = this.getOwnerComponent().getModel();
+                // oModel.setHeaders({
+                //     "sap-uname" : 'aItemArray[0]'
+                // });
+                // console.log(oModel);
                 this._oCurrentP13nData = null;
 			    this._bIsOpen = false;
                 var oMultiInput1 = this.getView().byId("multiInput");
@@ -30,7 +36,10 @@ sap.ui.define([
 				new Token({text: "555", key: "0002"}),
 			]);
 			this._mViewSettingsDialogs = {};
-
+                var that = this
+                this._pdfViewer = new PDFViewer();
+                this.getView().addDependent(this._pdfViewer);
+                console.log(this._pdfViewer);
                 var oTable = this.getView().byId('table0')
                 var oItemTemplate = new sap.m.ColumnListItem({
                     cells: [new sap.m.Text("text2",{text:"{Uname}"}), 
@@ -44,7 +53,46 @@ sap.ui.define([
                             new sap.m.Text("text5",{text:"{Mandt}"}),
                             new sap.m.Text("text6",{text:"{Modno}"}),
                             new sap.m.Text("text7",{text:"{Seqno}"}),
-                            new sap.m.Text("text9",{text:"{Xhold}"}) ]});
+                            new sap.m.Text("text9",{text:"{Xhold}"}) ]}
+                            ).addEventDelegate({
+                                onfocusin: function(oEvent) {
+                                    var aItemArray = []
+                                    // console.log(oModel);
+                                    console.log(oEvent.srcControl);
+                                    var sPath = "/FILE_UPLOADSet(FILENAME='PDF')/$value?"
+                                    var oSelectedRow = oEvent.srcControl.getCells()
+                                    for (var i = 0; i < oSelectedRow.length; i++) {
+                                        var vKey = oSelectedRow[i].mBindingInfos.text.parts[0].path
+                                        var vCell = oSelectedRow[i].getText()
+                                        if (vKey == 'Datum') {
+                                            vCell = vCell.replace("-", "")
+                                            vCell = vCell.replace("-", "")
+                                        }
+                                        if (vKey == 'Uzeit') {
+                                            vCell = vCell.replace(":", "")
+                                            vCell = vCell.replace(":", "")
+                                        }
+                                        console.log(vCell, vKey);
+                                        sPath = sPath + vKey + "=" + vCell + ","
+                                        aItemArray.push(vCell)
+                                    }
+                                    console.log(aItemArray[0]);
+
+                                    // var sPath = "/FILE_UPLOADSet(FILENAME='PDF')/$value?gch"
+                                    console.log(sPath);
+                                    var sSource = "/sap/opu/odata/sap/ZGC_SHOW_DUMPS_ODATA_SRV" + sPath
+                                    that._pdfViewer.setSource(sSource);
+                                    that._pdfViewer.setTitle(sSource);
+                                    that._pdfViewer.addCustomData()
+                                    
+                                    // that._pdfViewer.getModel().setHeaders({
+                                    //     "sap-uname" : 'aItemArray[0]'
+                                    // });
+                                    console.log(that._pdfViewer);
+                                    that._pdfViewer.open();
+                                }
+                            })
+                            console.log(oItemTemplate);
                 oTable.bindAggregation("items", {
                     path: "/SHOW_DUMPSSet",
                     filters: this.getFilter(),
